97f9f2753d8b96ac0cdc54f8c16fd51045a29286
Yannick Bock <bumbs@dings.bumbs>
2018-04-13 21:07:30 +0200

    ProbeController Unittests added.

---
+++ AnAusAutomat.Controllers.Probe.Tests/ProbeControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+
+namespace AnAusAutomat.Controllers.Probe.Tests
+{
+    public class ProbeControllerTests
+    {
+        // Current no functionality. ProbeController is just for testing purpose.
+
+        [Fact]
+        public void Connect()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Disconnect()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void TurnOff()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void TurnOn()
+        {
+            Assert.True(true);
+        }
+    }
+}
