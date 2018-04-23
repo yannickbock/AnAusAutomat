8c6cd76c752bf854b6896401fd0e3957c7a557d5
Yannick Bock <dings@bums.ding>
2018-04-23 11:26:13 +0200

    XmlSchemaValidatorTests added.

---
+++ AnAusAutomat.Toolbox.Tests/Xml/XmlSchemaValidatorTests.cs
@@ -0,0 +1,118 @@
+﻿using AnAusAutomat.Toolbox.Xml;
+using Serilog;
+using Serilog.Sinks.TestCorrelator;
+using System.Linq;
+using Xunit;
+
+namespace AnAusAutomat.Toolbox.Tests.Xml
+{
+    public class XmlSchemaValidatorTests
+    {
+        [Fact]
+        public void Validate_SchemaFileAndConfigFileValid()
+        {
+            setupLogger();
+
+            using (TestCorrelator.CreateContext())
+            {
+                var validator = new XmlSchemaValidator(schemaFilePath: "_TestData\\config_valid.xsd", configFilePath: "_TestData\\config_valid.xml");
+
+                bool isValid = validator.Validate();
+                Assert.True(isValid);
+
+                Assert.False(logMessageIsWritten(validator.SchemaFileNotFoundLogMessage));
+                Assert.False(logMessageIsWritten(validator.ConfigFileNotFoundLogMessage));
+                Assert.False(logMessageIsWritten(validator.SchemaNotValidLogMessage));
+                Assert.False(logMessageIsWritten(validator.ConfigNotValidLogMessage));
+            }
+        }
+
+        [Fact]
+        public void Validate_SchemaFileNotFound()
+        {
+            setupLogger();
+
+            using (TestCorrelator.CreateContext())
+            {
+                var validator = new XmlSchemaValidator(schemaFilePath: "", configFilePath: "_TestData\\config_valid.xml");
+
+                bool isValid = validator.Validate();
+                Assert.False(isValid);
+
+                Assert.True(logMessageIsWritten(validator.SchemaFileNotFoundLogMessage)); // <--
+                Assert.False(logMessageIsWritten(validator.ConfigFileNotFoundLogMessage));
+                Assert.False(logMessageIsWritten(validator.SchemaNotValidLogMessage));
+                Assert.False(logMessageIsWritten(validator.ConfigNotValidLogMessage));
+            }
+        }
+
+        [Fact]
+        public void Validate_ConfigFileNotFound()
+        {
+            setupLogger();
+
+            using (TestCorrelator.CreateContext())
+            {
+                var validator = new XmlSchemaValidator(schemaFilePath: "_TestData\\config_valid.xsd", configFilePath: "");
+
+                bool isValid = validator.Validate();
+                Assert.False(isValid);
+
+                Assert.False(logMessageIsWritten(validator.SchemaFileNotFoundLogMessage));
+                Assert.True(logMessageIsWritten(validator.ConfigFileNotFoundLogMessage)); // <--
+                Assert.False(logMessageIsWritten(validator.SchemaNotValidLogMessage));
+                Assert.False(logMessageIsWritten(validator.ConfigNotValidLogMessage));
+            }
+        }
+
+        [Fact]
+        public void Validate_SchemaNotValid()
+        {
+            setupLogger();
+
+            using (TestCorrelator.CreateContext())
+            {
+                var validator = new XmlSchemaValidator(schemaFilePath: "_TestData\\config_invalid.xsd", configFilePath: "_TestData\\config_valid.xml");
+
+                bool isValid = validator.Validate();
+                Assert.False(isValid);
+
+                Assert.False(logMessageIsWritten(validator.SchemaFileNotFoundLogMessage));
+                Assert.False(logMessageIsWritten(validator.ConfigFileNotFoundLogMessage));
+                Assert.True(logMessageIsWritten(validator.SchemaNotValidLogMessage)); // <--
+                Assert.False(logMessageIsWritten(validator.ConfigNotValidLogMessage));
+            }
+        }
+
+        [Fact]
+        public void Validate_ConfigNotValid()
+        {
+            setupLogger();
+
+            using (TestCorrelator.CreateContext())
+            {
+                var validator = new XmlSchemaValidator(schemaFilePath: "_TestData\\config_valid.xsd", configFilePath: "_TestData\\config_invalid.xml");
+
+                bool isValid = validator.Validate();
+                Assert.False(isValid);
+
+                Assert.False(logMessageIsWritten(validator.SchemaFileNotFoundLogMessage));
+                Assert.False(logMessageIsWritten(validator.ConfigFileNotFoundLogMessage));
+                Assert.False(logMessageIsWritten(validator.SchemaNotValidLogMessage));
+                Assert.True(logMessageIsWritten(validator.ConfigNotValidLogMessage)); // <--
+            }
+        }
+
+        private void setupLogger()
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.TestCorrelator().CreateLogger();
+        }
+
+        private bool logMessageIsWritten(string text)
+        {
+            int count = TestCorrelator.GetLogEventsFromCurrentContext().Count(x => x.MessageTemplate.Text == text);
+
+            return count == 1;
+        }
+    }
+}
