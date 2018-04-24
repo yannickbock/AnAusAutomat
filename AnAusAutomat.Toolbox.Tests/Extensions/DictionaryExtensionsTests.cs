35f60ed3f001094115c595a72378c2907185876c
Yannick Bock <dings@bums.ding>
2018-04-24 21:32:59 +0200

    DictionaryExtensions.GetValueOrDefault() added.

---
+++ AnAusAutomat.Toolbox.Tests/Extensions/DictionaryExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using AnAusAutomat.Toolbox.Extensions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace AnAusAutomat.Toolbox.Tests.Extensions
+{
+    public class DictionaryExtensionsTests
+    {
+        [Fact]
+        public void GetValueOrDefault()
+        {
+            var dictionary = new Dictionary<string, int>()
+            {
+                { "One", 1 },
+                { "Two", 2 },
+            };
+
+            Assert.Equal(3, dictionary.GetValueOrDefault("Three", 3));
+        }
+
+        [Fact]
+        public void GetValueOrDefault_DictionaryIsNull()
+        {
+            Dictionary<string, int> dictionary = null;
+
+            Assert.Throws<ArgumentNullException>(() => { dictionary.GetValueOrDefault("Three", 3); });
+        }
+
+        [Fact]
+        public void GetValueOrDefault_KeyIsNull()
+        {
+            var dictionary = new Dictionary<string, int>()
+            {
+                { "One", 1 },
+                { "Two", 2 },
+            };
+
+            Assert.Throws<ArgumentNullException>(() => { dictionary.GetValueOrDefault(null, 3); });
+        }
+    }
+}
---
+++ AnAusAutomat.Toolbox/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AnAusAutomat.Toolbox.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static TValue GetValueOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue = default(TValue))
+        {
+            if (dictionary == null)
+            {
+                throw new ArgumentNullException(nameof(dictionary));
+            }
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            return dictionary.TryGetValue(key, out TValue value) ? value : defaultValue;
+        }
+    }
+}
