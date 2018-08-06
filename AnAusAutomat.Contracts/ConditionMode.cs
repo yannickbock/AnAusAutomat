e1973f0afefc7cd0b44372d07333cc6f2dd57fdd
Yannick Bock <dings@bums.ding>
2018-08-06 19:43:47 +0200

    Added GetHashCode() and Equals() method for ConditionMode.

--- AnAusAutomat.Contracts/ConditionMode.cs
+++ AnAusAutomat.Contracts/ConditionMode.cs
@@ -11,5 +11,15 @@
         public string Name { get; private set; }
 
         public bool IsActive { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() + IsActive.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return this.GetHashCode() == obj.GetHashCode();
+        }
     }
 }
