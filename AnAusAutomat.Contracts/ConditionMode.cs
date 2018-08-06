08903482375f0c43dd54de30d32ff34f762c3010
Yannick Bock <dings@bums.ding>
2018-08-06 14:06:41 +0200

    Added missing constructor.

--- AnAusAutomat.Contracts/ConditionMode.cs
+++ AnAusAutomat.Contracts/ConditionMode.cs
@@ -2,6 +2,12 @@
 {
     public class ConditionMode
     {
+        public ConditionMode(string name, bool isActive)
+        {
+            Name = name;
+            IsActive = isActive;
+        }
+
         public string Name { get; private set; }
 
         public bool IsActive { get; set; }
