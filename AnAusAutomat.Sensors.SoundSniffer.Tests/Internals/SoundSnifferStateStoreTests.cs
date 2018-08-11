5a9b41052cf0a6baf54c32e3762be4953f5cdee0
Yannick Bock <dings@bums.ding>
2018-08-11 20:43:27 +0200

    Added SoundSnifferStateStore.

---
+++ AnAusAutomat.Sensors.SoundSniffer.Tests/Internals/SoundSnifferStateStoreTests.cs
@@ -0,0 +1,50 @@
+﻿using AnAusAutomat.Contracts;
+using AnAusAutomat.Sensors.SoundSniffer.Internals;
+using System;
+using Xunit;
+
+namespace AnAusAutomat.Sensors.SoundSniffer.Tests.Internals
+{
+    public class SoundSnifferStateStoreTests
+    {
+        [Fact]
+        public void GetSettings_DefaultValue()
+        {
+            var socket = new Socket(1, "Test");
+            var defaultSettings = SoundSnifferSettings.GetDefault();
+            var stateStore = new SoundSnifferStateStore();
+
+            Assert.Equal(defaultSettings, stateStore.GetSettings(socket));
+        }
+
+        [Fact]
+        public void GetSettings_SetAndGet()
+        {
+            var socket = new Socket(1, "Test");
+            var settings = new SoundSnifferSettings(TimeSpan.FromSeconds(50), TimeSpan.FromSeconds(7));
+            var stateStore = new SoundSnifferStateStore();
+            stateStore.SetSettings(socket, settings);
+
+            Assert.Equal(settings, stateStore.GetSettings(socket));
+        }
+
+        [Fact]
+        public void GetStatus_DefaultValue()
+        {
+            var socket = new Socket(1, "Test");
+            var stateStore = new SoundSnifferStateStore();
+
+            Assert.Equal(PowerStatus.Undefined, stateStore.GetStatus(socket));
+        }
+
+        [Fact]
+        public void GetStatus_SetAndGet()
+        {
+            var socket = new Socket(1, "Test");
+            var stateStore = new SoundSnifferStateStore();
+            stateStore.SetStatus(socket, PowerStatus.On);
+
+            Assert.Equal(PowerStatus.On, stateStore.GetStatus(socket));
+        }
+    }
+}
---
+++ AnAusAutomat.Sensors.SoundSniffer/Internals/SoundSnifferStateStore.cs
@@ -0,0 +1,37 @@
+﻿using AnAusAutomat.Contracts;
+using System.Collections.Generic;
+
+namespace AnAusAutomat.Sensors.SoundSniffer.Internals
+{
+    public class SoundSnifferStateStore
+    {
+        private Dictionary<Socket, PowerStatus> _states;
+        private Dictionary<Socket, SoundSnifferSettings> _settings;
+
+        public SoundSnifferStateStore()
+        {
+            _states = new Dictionary<Socket, PowerStatus>();
+            _settings = new Dictionary<Socket, SoundSnifferSettings>();
+        }
+
+        public SoundSnifferSettings GetSettings(Socket socket)
+        {
+            return _settings.ContainsKey(socket) ? _settings[socket] : SoundSnifferSettings.GetDefault();
+        }
+
+        public void SetSettings(Socket socket, SoundSnifferSettings settings)
+        {
+            _settings[socket] = settings;
+        }
+
+        public PowerStatus GetStatus(Socket socket)
+        {
+            return _states.ContainsKey(socket) ? _states[socket] : PowerStatus.Undefined;
+        }
+
+        public void SetStatus(Socket socket, PowerStatus status)
+        {
+            _states[socket] = status;
+        }
+    }
+}
