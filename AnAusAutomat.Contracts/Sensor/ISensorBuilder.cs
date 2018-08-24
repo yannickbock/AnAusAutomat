d9575a75e4ceaec7d79916e9c18298f4d06f90c7
Yannick Bock <dings@bums.ding>
2018-08-24 17:12:17 +0200

    Added ISensorBuilder interface.

---
+++ AnAusAutomat.Contracts/Sensor/ISensorBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace AnAusAutomat.Contracts.Sensor
+{
+    public interface ISensorBuilder
+    {
+        void AddParameter(SensorParameter parameter);
+
+        void AddSocket(Socket socket, IEnumerable<SensorParameter> parameters);
+
+        void AddMode(ConditionMode mode);
+
+        ISensor Build();
+    }
+}
